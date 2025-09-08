41514c4190f7b76c51a19131a467d473eb9f779d
Qirat-khalil <qiratkhalil39@gmail.com>
2025-09-08 09:22:14 -0700

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,4 @@
 
-// import Image from "next/image";
-// inshara
-import Navbar from "./Component/Navbar";
-import Section1 from "./Component/Section1";
-import Section2 from "./Component/Section2";
-import Section3 from "./Component/Section3";
-import Section4 from "./Component/Section4";
-import Section5 from "./Component/Section5";
-
-
 import PricingSection from "./Component/Pricing";
 import FAQSection from "./Component/FAQsection";
 import Integrations from "./Component/Integrations";
@@ -28,16 +18,8 @@ export default function Home() {
       <MernSection/>
       <WordpressSection />
       <MobileSection/>
-      <Navbar />
-      <Section1 />
-      <Section2 />
       <BenefitsSection />
-      <Section3 />
-      <Section4 />
       <Carousel />
-      <Section5 />
-
-
       <Integrations />
       <PricingSection />
       <FAQSection />
