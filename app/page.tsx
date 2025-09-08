bad8b0cefa32e710e32e496061d7d2646d35c200
insharahkalam <insharahkalam19@gmail.com>
2025-09-08 21:34:35 +0500

    resolve merge conflicts and combine code

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,3 @@
-// <<<<<<< HEAD
-// =======
-
-// import PricingSection from "./Component/Pricing";
-// import FAQSection from "./Component/FAQsection";
-// import Integrations from "./Component/Integrations";
-// import BenefitsSection from "./Component/Benefit";
-// import ReviewSection from "./Component/Reviews";
-// import TeamSection from "./Component/OurAmazinteam";
-// import Carousel from "./Component/Carousel";
-
-// import WordpressSection from "./Component/Wordpressportfolio";
-// import MernSection from "./Component/Mernportfolio";
-// import MobileSection from "./Component/Mobileportfolio";
-
-// >>>>>>> 41514c4190f7b76c51a19131a467d473eb9f779d
-
-// import Slider from "./Component/Slider";
-// import TestimonialSection from "./Component/TestimonialSection";
-// import AllFeatureSection from "./Component/AllFeatureSection";
-// import ProcessSection from "./Component/ProcessSection";
-// import ServiceSection2 from "./Component/ServiceSection2";
-// import ComparisionSection from "./Component/ComparisionSection";
-// import ReachOut from "./Component/ReachOut";
-// import ServiceSection from "./Component/ServiceSection";
-
-// function Home() {
-//   return (
-//     <>
-// <<<<<<< HEAD
-//       <Slider />
-//       <TestimonialSection />
-//       <AllFeatureSection />
-//       <ProcessSection />
-//       <ServiceSection />
-//       <ServiceSection2 />
-//       <ComparisionSection />
-//       <ReachOut />
-   
-
-//     </>
-//   );
-// }
-
-// export default Home
-// =======
-//       <MernSection/>
-//       <WordpressSection />
-//       <MobileSection/>
-//       <BenefitsSection />
-//       <Carousel />
-//       <Integrations />
-//       <PricingSection />
-//       <FAQSection />
-//       <ReviewSection />
-//       <TeamSection />
-
-//     </>
-//   )
-// }
-// >>>>>>> 41514c4190f7b76c51a19131a467d473eb9f779d
-
 
 
 import PricingSection from "./Component/Pricing";
@@ -74,6 +12,8 @@ import WordpressSection from "./Component/Wordpressportfolio";
 import MernSection from "./Component/Mernportfolio";
 import MobileSection from "./Component/Mobileportfolio";
 
+
+
 import Slider from "./Component/Slider";
 import TestimonialSection from "./Component/TestimonialSection";
 import AllFeatureSection from "./Component/AllFeatureSection";
@@ -86,7 +26,7 @@ import ServiceSection from "./Component/ServiceSection";
 function Home() {
   return (
     <>
-      {/* Tumhara code */}
+
       <Slider />
       <TestimonialSection />
       <AllFeatureSection />
@@ -95,11 +35,11 @@ function Home() {
       <ServiceSection2 />
       <ComparisionSection />
       <ReachOut />
+   
 
-      {/* Qirat ka code */}
-      <MernSection />
+        <MernSection/>
       <WordpressSection />
-      <MobileSection />
+      <MobileSection/>
       <BenefitsSection />
       <Carousel />
       <Integrations />
@@ -107,8 +47,65 @@ function Home() {
       <FAQSection />
       <ReviewSection />
       <TeamSection />
+
     </>
   );
 }
 
-export default Home;
+export default Home
+
+ 
+
+
+
+
+// import PricingSection from "./Component/Pricing";
+// import FAQSection from "./Component/FAQsection";
+// import Integrations from "./Component/Integrations";
+// import BenefitsSection from "./Component/Benefit";
+// import ReviewSection from "./Component/Reviews";
+// import TeamSection from "./Component/OurAmazinteam";
+// import Carousel from "./Component/Carousel";
+
+// import WordpressSection from "./Component/Wordpressportfolio";
+// import MernSection from "./Component/Mernportfolio";
+// import MobileSection from "./Component/Mobileportfolio";
+
+// import Slider from "./Component/Slider";
+// import TestimonialSection from "./Component/TestimonialSection";
+// import AllFeatureSection from "./Component/AllFeatureSection";
+// import ProcessSection from "./Component/ProcessSection";
+// import ServiceSection2 from "./Component/ServiceSection2";
+// import ComparisionSection from "./Component/ComparisionSection";
+// import ReachOut from "./Component/ReachOut";
+// import ServiceSection from "./Component/ServiceSection";
+
+// function Home() {
+//   return (
+//     <>
+//       {/* Tumhara code */}
+//       <Slider />
+//       <TestimonialSection />
+//       <AllFeatureSection />
+//       <ProcessSection />
+//       <ServiceSection />
+//       <ServiceSection2 />
+//       <ComparisionSection />
+//       <ReachOut />
+
+//       {/* Qirat ka code */}
+//       <MernSection />
+//       <WordpressSection />
+//       <MobileSection />
+//       <BenefitsSection />
+//       <Carousel />
+//       <Integrations />
+//       <PricingSection />
+//       <FAQSection />
+//       <ReviewSection />
+//       <TeamSection />
+//     </>
+//   );
+// }
+
+// export default Home;
